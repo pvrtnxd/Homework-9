0e2eb4be40ea6dd7bc2560107b25a37510ffb1dd
lebed <lebedamm@gmail.com>
2022-12-19 17:27:55 +0300

    Zadanie 9

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        String firstName = "Marina";
+        String middleName = "Maksimovna";
+        String lastName = "Lebedeva";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("Задание 2");
+
+        String upper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upper);
+
+        System.out.println("Задание 3");
+
+        String fullName0 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрулника - " + fullName0.replace("ё", "е"));
+
+    }
+}
